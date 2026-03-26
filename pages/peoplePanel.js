4f9ca34221c3d04e45799ba5b543709e6e37bb50
ashmitasur <ashmita.sur@kreeti.com>
2026-03-26 11:30:48 +0530

    resume parse

--- pages/peoplePanel.js
+++ pages/peoplePanel.js
@@ -6,10 +6,14 @@ export class PeoplePanel {
     this.keywordSearchBox = page.locator('#text-search-input');
     this.personInList = page.locator('.people-grid-view__person-name__link');
     this.candidateInProjectCrad = page.locator('.candidate-in-projects');
+    this.documentCard = page.locator("#resume-overview-card");
     this.cardExpandCollapseButton = page.locator('.card-view__collapse-button');
     this.collapsedCardAccordian='.card-view__header--collapsed'
     this.addRecordBtnLocator = '.add-record-btn';
-
+    this.uploadInput = page.locator('.dz-hidden-input');
+    this.documentTitle = page.locator('#title');
+    this.documentType = 'div.select-option-floating';
+    this.resumeparseCheckbox = page.getByLabel('Parse')
   }
 
 
@@ -39,7 +43,6 @@ export class PeoplePanel {
         await this.cardExpandCollapseButton.click()
     }
     const addBtn = this.candidateInProjectCrad.locator(this.addRecordBtnLocator);
-
     await expect(addBtn).toBeVisible();
     await addBtn.click();
     await this.searchNSelect(searchToSelectProject)
@@ -53,6 +56,29 @@ export class PeoplePanel {
     await expect(this.page.locator('.search-list-dropdown__search')).toBeVisible()
     await this.page.locator('.search-list-dropdown__search > div > ul > li:nth-child(4)').click()
   }
+
+  async uploadResume(filepath,title){
+    const cardCollapsed = this.documentCard.locator(this.collapsedCardAccordian);
+    if(cardCollapsed.count() > 0){
+        await this.cardExpandCollapseButton.click()
+    }
+    const addBtn = this.documentCard.locator(this.addRecordBtnLocator);
+    await expect(addBtn).toBeVisible();
+    await addBtn.click();
+    await this.uploadInput.setInputFiles(filepath)
+    await this.documentTitle.fill(title)
+    await this.documentCard.locator(this.documentType).click()
+    await this.singleSelectDD(this.documentCard)
+    await this.resumeparseCheckbox.click()
+    await this.documentCard.locator('[type="submit"]').click()
+    await expect(this.documentCard.locator('[type="submit"]')).toBeDisabled()
+
+  }
+
+  async singleSelectDD(container){
+    await container.locator('.list-items > [role="option"]:nth-child(1)').click()
+  }
+
   async closePeoplePanle(){
     await this.page.locator('[class="side-panel__controls__item close-item"]').click()
   }
