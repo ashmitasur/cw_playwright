fe609c26750ec30e956525a2404aa70a63992c71
ashmitasur <ashmita.sur@kreeti.com>
2026-04-28 12:38:57 +0530

    add project team

---
+++ tests/addAndRemoveProjectTeam.spec.js
@@ -0,0 +1,48 @@
+import { test, expect, chromium } from '@playwright/test';
+import { LogInPage } from '../pages/logInPage';
+import { credentials } from '../testData/credentials';
+import { NavigationPage } from '../pages/navigationPage';
+import { ProjectPage } from '../pages/project';
+import { staticdata } from '../testData/staticdata';
+
+let browser;
+let context;
+let page;
+let loginpage;
+let navmenu;
+let envData;
+let projectpage;
+
+test.describe('Project Team Member',() =>{
+    test.beforeAll(async() =>{
+        browser = await chromium.launch({headless: false})
+        context = await browser.newContext()
+        page = await context.newPage()
+
+        loginpage = new LogInPage(page)
+        navmenu = new NavigationPage(page)
+        projectpage = new ProjectPage(page)
+        const env = process.env.PLATFORM
+        envData = credentials[env]
+        const {firmname,accountUrl} = envData
+
+        await loginpage.gotoAccountsPage(accountUrl)
+        await loginpage.selectFirm(firmname)
+    })
+
+    test("Add Team Member", async({},testInfo)=>{
+        await navmenu.goToPage('projects/gridview')
+        await expect(page).toHaveURL(`${testInfo.project.use.baseURL}`+'/firm/projects/gridview')  
+        const{searchProject,searchprojectMember} = staticdata      
+        const newPage = await projectpage.openProject(context,searchProject)
+        await projectpage.openSection(newPage,'dashboard')
+        expect(await newPage.url()).toContain('dashboard')
+        await projectpage.addTeam(newPage,searchprojectMember)
+    })
+
+     test.afterAll(async({},testInfo) =>{
+        //logout
+        await loginpage.logout()
+        await expect(page).toHaveURL(`${testInfo.project.use.baseURL}`+'/session/new')
+    })   
+})
